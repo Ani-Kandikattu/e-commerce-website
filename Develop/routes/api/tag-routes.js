d3f8ea5d29a9265119d00f026936756e7aa6e2ac
Ani-Kandikattu <anikandikattu@gmail.com>
2023-04-27 17:14:01 -0700

    finished get method and added code for beginning of post, put and delete methods

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -16,21 +16,44 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findOne(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }]
+    });
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      
+    })
+  } catch(err) {}
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update({
+      
+    })
+  } catch(err) {}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      
+    })
+  } catch(err) {}
 });
 
 module.exports = router;
